14f70760f2233e6ccd04025d7785e0afbec69729
gioezzy <giovanniyuda29@gmail.com>
2025-09-24 00:05:10 +0700

    feat: add API Gateway service with Spring Cloud Gateway

---
+++ api-gateway-pustaka/src/main/java/com/gio/api_gateway_pustaka/ApiGatewayPustakaApplication.java
@@ -0,0 +1,13 @@
+package com.gio.api_gateway_pustaka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayPustakaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayPustakaApplication.class, args);
+	}
+
+}
---
+++ api-gateway-pustaka/src/test/java/com/gio/api_gateway_pustaka/ApiGatewayPustakaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gio.api_gateway_pustaka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayPustakaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
