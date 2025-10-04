853a8a5a09fda202ffd0d1a375c23925a154df18
gioezzy <giovanniyuda29@gmail.com>
2025-10-04 22:08:15 +0700

    feat: initilize service for rabbimq pengembalian

---
+++ rabbitmq-pengembalian-service/src/test/java/com/gio/rabbitmq_pengembalian_service/RabbitmqPengembalianServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gio.rabbitmq_pengembalian_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqPengembalianServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
