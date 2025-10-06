b2f2582d416d167a35d1bf8e271c3c60e5e983d4
gioezzy <giovanniyuda29@gmail.com>
2025-10-06 22:14:58 +0700

    feat: implement consumer to receive and process messages from queue

---
+++ rabbitmq-pengembalian-service/src/main/java/com/gio/rabbitmq_pengembalian_service/PengembalianConsumerService.java
@@ -0,0 +1,58 @@
+package com.gio.rabbitmq_pengembalian_service;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
+
+import com.gio.rabbitmq_pengembalian_service.vo.Anggota;
+import com.gio.rabbitmq_pengembalian_service.vo.Pengembalian;
+import com.gio.rabbitmq_pengembalian_service.vo.ResponseTemplate;
+
+@Service
+public class PengembalianConsumerService {
+
+  @Autowired
+  private JavaMailSender javaMailSender;
+
+  @Autowired
+  private DiscoveryClient discoveryClient;
+
+  @Autowired
+  private RestTemplate restTemplate;
+
+  @Value("${app.mail.from}")
+  private String from;
+
+  @RabbitListener(queues = "${app.rabbitmq-pengembalian.queue}")
+
+  @Transactional
+  public void receiveOrder(@Payload Pengembalian pengembalian){
+    System.out.println("Sending receive to email: " + pengembalian.getId());
+
+    try{
+      ServiceInstance serviceInstance = discoveryClient.getInstances("API-GATEWAY-PUSTAKA").get(0);
+      ResponseTemplate[] response = restTemplate.getForObject(serviceInstance.getUri() + "/api/pengembalian/" + pengembalian.getId() + "/detail", ResponseTemplate[].class);
+      ResponseTemplate dataPengembalian = response[0];
+      Anggota anggota = dataPengembalian.getAnggota();
+      String email = anggota.getEmail();
+
+      System.out.println("Sending notification to email:" + email);
+      SimpleMailMessage mailMessage = new SimpleMailMessage();
+      mailMessage.setFrom(from);
+      mailMessage.setTo(email);
+      mailMessage.setText(dataPengembalian.sendMailMessage());
+      mailMessage.setSubject("Konfirmasi Pengembalian Buku Berhasil");
+      javaMailSender.send(mailMessage);
+    }catch (Exception e){
+      System.out.println(e.toString());
+    }
+  }
+}
