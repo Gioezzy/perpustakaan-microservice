faa572ef4c647a70f9819022fafecd010f953f0b
gioezzy <giovanniyuda29@gmail.com>
2025-10-08 22:10:11 +0700

    feat: setup initial project with CQRS dependencies

---
+++ peminjaman-command-service/src/main/java/com/gio/peminjaman_command_service/PeminjamanCommandServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gio.peminjaman_command_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeminjamanCommandServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeminjamanCommandServiceApplication.class, args);
+	}
+
+}
---
+++ peminjaman-command-service/src/test/java/com/gio/peminjaman_command_service/PeminjamanCommandServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gio.peminjaman_command_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeminjamanCommandServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
