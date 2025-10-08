d60dff29e9ad7ad761a085529e478412593f8079
gioezzy <giovanniyuda29@gmail.com>
2025-10-08 22:10:35 +0700

    feat: setup initial project with CQRS dependencies

---
+++ peminjaman-query-service/src/main/java/com/gio/peminjaman_query_service/PeminjamanQueryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gio.peminjaman_query_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeminjamanQueryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeminjamanQueryServiceApplication.class, args);
+	}
+
+}
