19c55540210736036b17f732b2c490cb70254851
gioezzy <giovanniyuda29@gmail.com>
2025-10-14 23:56:13 +0700

    feat: add data models

---
+++ peminjaman-command-service/src/main/java/com/gio/peminjaman_command_service/model/Peminjaman.java
@@ -0,0 +1,19 @@
+package com.gio.peminjaman_command_service.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "peminjamans")
+public class Peminjaman {
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private Long bukuId;
+  private Long anggotaId;
+  private String tanggalPinjam;
+  private String tanggalKembali;
+
+}
