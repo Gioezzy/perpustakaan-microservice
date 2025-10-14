91d4e973d7ac506a1d2254416a7578d50622f1e8
gioezzy <giovanniyuda29@gmail.com>
2025-10-14 23:56:13 +0700

    feat: add RabbitMQ listeners

---
+++ peminjaman-command-service/src/main/java/com/gio/peminjaman_command_service/listener/PeminjamanEventListener.java
@@ -0,0 +1,13 @@
+package com.gio.peminjaman_command_service.listener;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PeminjamanEventListener {
+
+    @RabbitListener(queues = "${app.rabbitmq-peminjaman.queue}")
+    public void handlePeminjamanEvent(Object message) {
+        System.out.println("📨 Received event: " + message.toString());
+    }
+}
