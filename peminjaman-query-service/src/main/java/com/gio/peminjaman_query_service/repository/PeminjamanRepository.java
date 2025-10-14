3d75e21e5abbf2a38c7002b14f2af004ac935766
gioezzy <giovanniyuda29@gmail.com>
2025-10-14 23:56:13 +0700

    feat: add repository layer

---
+++ peminjaman-query-service/src/main/java/com/gio/peminjaman_query_service/repository/PeminjamanRepository.java
@@ -0,0 +1,9 @@
+package com.gio.peminjaman_query_service.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.gio.peminjaman_query_service.model.Peminjaman;
+
+@Repository
+public interface PeminjamanRepository extends JpaRepository<Peminjaman, Long> {
+}
