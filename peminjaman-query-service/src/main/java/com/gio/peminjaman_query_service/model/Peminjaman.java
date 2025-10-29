dae1da1fe86b554c7ed101761f98d4d6f9053d3d
gioezzy <giovanniyuda29@gmail.com>
2025-10-29 21:49:16 +0700

    refactor: add Lombok annotations and toString to Peminjaman model

--- peminjaman-query-service/src/main/java/com/gio/peminjaman_query_service/model/Peminjaman.java
+++ peminjaman-query-service/src/main/java/com/gio/peminjaman_query_service/model/Peminjaman.java
@@ -1,23 +1,37 @@
 package com.gio.peminjaman_query_service.model;
 
-import jakarta.persistence.*;
-import lombok.*;
+import java.util.Date;
 
-import java.time.LocalDate;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-@Table(name = "peminjaman_query")
+@Table(name = "peminjamans")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class Peminjaman {
     @Id
     private Long id;
-
     private Long bukuId;
     private Long anggotaId;
-    private LocalDate tanggalPinjam;
-    private LocalDate tanggalKembali;
+    private Date tanggalPinjam;
+    private Date tanggalKembali;
+
+    @Override
+    public String toString() {
+        return "Peminjaman{" +
+                "id=" + id +
+                ", bukuId=" + bukuId +
+                ", anggotaId=" + anggotaId +
+                ", tanggalPinjam=" + tanggalPinjam +
+                ", tanggalKembali=" + tanggalKembali +
+                '}';
+    }
 }
