9c369433df6458e038c92d37cc5ce58ce854c905
gioezzy <giovanniyuda29@gmail.com>
2025-10-29 21:48:46 +0700

    feat: add detail endpoint to PeminjamanController

--- peminjaman-query-service/src/main/java/com/gio/peminjaman_query_service/controller/PeminjamanController.java
+++ peminjaman-query-service/src/main/java/com/gio/peminjaman_query_service/controller/PeminjamanController.java
@@ -2,10 +2,12 @@ package com.gio.peminjaman_query_service.controller;
 
 import com.gio.peminjaman_query_service.model.Peminjaman;
 import com.gio.peminjaman_query_service.service.PeminjamanService;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
+// import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/peminjaman/query")
@@ -22,8 +24,8 @@ public class PeminjamanController {
         return service.findAll();
     }
 
-    @GetMapping("/{id}")
-    public Optional<Peminjaman> getById(@PathVariable Long id) {
-        return service.findById(id);
+    @GetMapping("/{id}/detail")
+    public ResponseEntity<Peminjaman> getDetail(@PathVariable Long id) {
+        return service.findById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
     }
 }
